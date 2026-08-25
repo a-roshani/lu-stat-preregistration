be5fb67375bc225d4b9a159d15e9fde2be41189c
Amin Roshani <roshani.amin@gmail.com>
2026-08-25 14:18:23 +0330

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// فقط این دو مقدار را از Supabase وارد کنید.
-// Publishable key برای استفاده در مرورگر طراحی شده است.
-// هرگز service_role key را در GitHub یا این فایل قرار ندهید.
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://exwpovakubjalrjznhry.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_yOTO5JEKLGgB9cmxL5YUkg_21vK1Thu"
-};
