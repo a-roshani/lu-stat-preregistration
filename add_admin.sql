437d4c27d6aad13781c32d6c78b0edb155b5cf0a
Amin Roshani <roshani.amin@gmail.com>
2026-08-25 14:22:08 +0330

    Update add_admin.sql

--- add_admin.sql
+++ add_admin.sql
@@ -4,7 +4,7 @@
 insert into public.admins(user_id)
 select id
 from auth.users
-where email = 'YOUR_ADMIN_EMAIL@example.com'
+where email = 'roshani.amin@gmail.com'
 on conflict (user_id) do nothing;
 
 -- کنترل نتیجه
